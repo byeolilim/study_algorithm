872d34a3f58c075da51521f40b43e19f5a136997
byeoli <98lim511@naver.com>
2021-11-19 15:25:49 +0900

    방법1_2

--- programmers/pg_1.java
+++ programmers/pg_1.java
@@ -1,4 +1,25 @@
-class Solution {
+//방법1
+class Solution1 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int[] sum = {0,1,2,3,4,5,6,7,8,9};
+
+        for(int i = 0; i< sum.length; i++){
+            for(int j=0; j< numbers.length; j++){
+                if(sum[i] == numbers[j]){
+                    sum[i] -= numbers[j];
+                }
+            }
+            answer += sum[i];
+        }
+        return answer;
+    }
+}
+
+
+
+//방법2
+class Solution2 {
     public int solution(int[] numbers) {
         int sum = 45; //0~9까지 합.
         
